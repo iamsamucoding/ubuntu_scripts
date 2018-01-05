37d436ab01621ff852811f141db2a2ac333e6ba9
Samuka XaveCoder <sbm.martins@gmail.com>
2018-01-05 11:17:02 -0200

    creating script for python packages installation

---
+++ install_python_packages.sh
@@ -0,0 +1,25 @@
+source ~/yeah/bin/activate
+
+printf "\n---> Installing Ipython\n"
+pip install ipython
+
+printf "\n---> Installing Numpy\n"
+pip install numpy
+
+printf "\n---> Installing Scipy\n"
+pip install scipy
+
+printf "\n---> Installing Matplotlib\n"
+pip install matplotlib
+
+printf "\n---> Installing Scikit-learn\n"
+pip install -U scikit-learn
+
+printf "\n---> iJupyter\n"
+pip install jupyter
+
+printf "\n---> Scikit-Image\n"
+pip install scikit-image
+
+printf "\n---> Installing Cython\n"
+sudo apt-get -y install cython
