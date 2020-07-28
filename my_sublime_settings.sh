c9a82ee4965fd66972f4e30d8891533d447cde53
Samuka Martins <sbm.martins@gmail.com>
2020-07-28 15:59:39 -0300

    adding flag -p to mkdir to avoid error 'Directory exists'

--- my_sublime_settings.sh
+++ my_sublime_settings.sh
@@ -1,5 +1,5 @@
-mkdir ~/.config/sublime-text-3
-mkdir ~/.config/sublime-text-3/Packages/Colorsublime-Themes
+mkdir -p ~/.config/sublime-text-3
+mkdir -p ~/.config/sublime-text-3/Packages/Colorsublime-Themes
 cp ./sublime/*.tmTheme ~/.config/sublime-text-3/Packages/Colorsublime-Themes
 cp ./sublime/my_settings.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
 cp ./sublime/my_keys.sublime-keymap ~/.config/sublime-text-3/Packages/User/Default\ \(Linux\).sublime-keymap
